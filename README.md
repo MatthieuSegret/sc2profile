2e0dd8d4105a62819858b78536085dea423ead32
Julian Møller <julian@julianmoeller.dk>
2011-03-30 01:05:11 +0200

    Initial commit to sc2stats.

---
+++ README.md
@@ -0,0 +1,19 @@
+# sc2stats
+
+Parses a Battle.net StarCraft 2 profile for name, 1v1 league and 1v1 league points.
+
+## Contributing to sc2stats
+ 
+* Check out the latest master to make sure the feature hasn't been implemented or the bug hasn't been fixed yet
+* Check out the issue tracker to make sure someone already hasn't requested it and/or contributed it
+* Fork the project
+* Start a feature/bugfix branch
+* Commit and push until you are happy with your contribution
+* Make sure to add tests for it. This is important so I don't break it in a future version unintentionally.
+* Please try not to mess with the Rakefile, version, or history. If you want to have your own version, or is otherwise necessary, that is fine, but please isolate to its own commit so I can cherry-pick around it.
+
+== Copyright
+
+Copyright (c) 2011 Julian Møller. See LICENSE.txt for
+further details.
+
---
+++ lib/sc2stats.rb
@@ -0,0 +1,95 @@
+require "rubygems"
+require "bundler/setup"
+
+require "nokogiri"
+require 'open-uri'
+
+class SC2League
+	def initialize(name)
+		index = LEAGUES.index(name)
+
+		if not index.nil?
+			@index = index
+			@name = name
+		end
+	end
+
+	LEAGUES = ["grand master", "master", "diamond", "platinum", "gold", "silver", "bronze", "no league"]
+
+	def leagues
+		@leagues
+	end
+
+	def to_s
+		@name.capitalize
+	end
+end
+
+class SC2Stats
+	def scrape
+		html = Nokogiri::HTML(open("#{@url}/ladder/leagues"))
+
+		@league = scrape_league(html)
+		@points = scrape_points(html)
+		@name = scrape_name(html)
+
+		true
+	end
+
+	def scrape_league(html)
+		league = nil
+
+		html.css("#profile-menu li a").each do |l|
+			if l.text.include? "1v1"
+				league = l
+				break
+			end
+		end
+
+		leaguename = league.text.strip.gsub(/\dv\d /, "").gsub(/ Rank.+/, "").downcase unless league.nil?
+		leaguename ||= SC2League::LEAGUES.last
+
+		SC2League.new(leaguename)
+	end
+
+	def scrape_points(html)
+		td = html.css("#current-rank td")[3]
+
+		if td.nil?
+			0
+		else
+			td.text
+		end
+	end
+
+	def scrape_name(html)
+		html.css("#profile-header h2 a").first.text
+	end
+
+	def initialize(profileurl)
+		@url = profileurl
+
+		self.scrape
+	end
+
+	def url; @url; end
+	def url=(value)
+		@url = value
+	end
+
+	def league; @league; end
+	def points; @points; end
+	def name; @name; end
+
+	def to_s
+		"#{@name} (#{@league}, #{@points} pts)"
+	end
+
+	def ==(o)
+		if o.is_a? SC2Stats
+			@url == o.url
+		else
+			false
+		end
+	end
+end
