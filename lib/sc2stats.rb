MatthieuSegret/sc2stats

- Add race and portrait_style - Bugs fix - Optionals informations
Signed-off-by: Matthieu <matthieu.segret@gmail.com>

lib/sc2stats.rb
@@ -3,93 +3,115 @@ require "bundler/setup"
 
 require "nokogiri"
 require 'open-uri'
-
-class SC2League
-	def initialize(name)
-		index = LEAGUES.index(name)
-
-		if not index.nil?
-			@index = index
-			@name = name
-		end
-	end
-
-	LEAGUES = ["no league", "bronze", "silver", "gold", "platinum", "diamond", "master", "grand master"]
-	
-	def leagues
-		@leagues
-	end
-
-	def to_s
-		@name.capitalize
-	end
-end
+require 'sc2league'
+require 'sc2portrait_style'
+require 'sc2race'
 
 class SC2Stats
-	def scrape
-		html = Nokogiri::HTML(open("#{@url}/ladder/leagues","User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0) Gecko/20100101 Firefox/4.0"))
+  
+  # --------------------------------------------------------------------------
+  #
+  # Accessor
+  #
+  # --------------------------------------------------------------------------
+  
+  attr_reader :name, :race, :points, :league, :portrait_style
+  attr_accessor :url, :options
+  
+  
+  # --------------------------------------------------------------------------
+  #
+  # Constructor method
+  #
+  # --------------------------------------------------------------------------
+  
+  def initialize(profileurl, options=[:name, :race, :points, :league, :portrait_style])
+    @url = profileurl
+    @options = options
+    self.scrape @options
+  end
+  
+  
+  # --------------------------------------------------------------------------
+  #
+  # Public methods
+  #
+  # --------------------------------------------------------------------------
+  
+  def to_s
+    "#{@name} (#{@league}, #{@race}, #{@points} pts)"
+  end
 
-		@league = scrape_league(html)
-		@points = scrape_points(html)
-		@name = scrape_name(html)
+  def ==(o)
+    if o.is_a? SC2Stats
+      @url == o.url
+    else
+      false
+    end
+  end
+  
+  def scrape(options)
+    @url.chop! if @url.last == '/'
+    html = Nokogiri::HTML(open("#{@url}/ladder/leagues"))
 
-		true
-	end
+    options.each do |option|
+      instance_variable_set("@#{option.to_s}".to_sym, self.send("scrape_#{option.to_s}", html))
+    end
+  end
+  
+  
+  # --------------------------------------------------------------------------
+  #
+  # Protected methods
+  #
+  # --------------------------------------------------------------------------
+  
+  protected
+  
+  def scrape_name(html)
+    html.css("#profile-header h2 a").first.text
+  end
+  
+  def scrape_portrait_style(html)
+    raw_portrait_style = html.css("#portrait span").attr('style').to_s
+    return '' if raw_portrait_style.blank?
+    style_split = raw_portrait_style.split
+    image_index = /.+\/(\d)-90.jpg.+/.match(style_split[1])[1]
+    image_x = style_split[2]
+    image_y = style_split[3]
+    SC2PortraitStyle.new(image_index, image_x, image_y)
+  end
+  
+  def scrape_race(html)
+    td = html.css("#current-rank td")[2]
+    return '' if td.blank?
+    class_race = td.css("a").attr('class').to_s
+    return '' if class_race.blank?
+    race = class_race.split('-')[1]
+    SC2Race.new(race)
+  end
+  
+  def scrape_points(html)
+    td = html.css("#current-rank td")[3]
+    td.present? ? td.text.to_i : 0
+  end
 
-	def scrape_league(html)
-		league = nil
+  def scrape_league(html, league_type='1c1')
+    league_text = nil
+    
+    html.css("#profile-menu li a").each do |l|
+      if l.text.include? "1c1"
+        league_text = l.text
+        break
+      end
+    end
 
-		html.css("#profile-menu li a").each do |l|
-			if l.text.include? "1v1"
-				league = l
-				break
-			end
-		end
+    if league_text.present?
+      league_split = league_text.downcase.split
+      league_name = league_split[0]
+      rang = league_split[3].to_i
+    end
 
-		leaguename = league.text.strip.gsub(/\dv\d /, "").gsub(/ Rank.+/, "").downcase unless league.nil?
-		leaguename ||= SC2League::LEAGUES.last
-
-		SC2League.new(leaguename)
-	end
-
-	def scrape_points(html)
-		td = html.css("#current-rank td")[3]
-
-		if td.nil?
-			0
-		else
-			Integer(td.text)
-		end
-	end
-
-	def scrape_name(html)
-		html.css("#profile-header h2 a").first.text
-	end
-
-	def initialize(profileurl)
-		@url = profileurl
-
-		self.scrape
-	end
-
-	def url; @url; end
-	def url=(value)
-		@url = value
-	end
-
-	def league; @league; end
-	def points; @points; end
-	def name; @name; end
-
-	def to_s
-		"#{@name} (#{@league}, #{@points} pts)"
-	end
-
-	def ==(o)
-		if o.is_a? SC2Stats
-			@url == o.url
-		else
-			false
-		end
-	end
+    SC2League.new(league_name, league_type, rang)
+  end
 end

MatthieuSegret/sc2profile

lib/sc2stats.rb
@@ -3,93 +3,126 @@ require "bundler/setup"
 
 require "nokogiri"
 require 'open-uri'
-
-class SC2League
-	def initialize(name)
-		index = LEAGUES.index(name)
-
-		if not index.nil?
-			@index = index
-			@name = name
-		end
-	end
-
-	LEAGUES = ["no league", "bronze", "silver", "gold", "platinum", "diamond", "master", "grand master"]
-	
-	def leagues
-		@leagues
-	end
-
-	def to_s
-		@name.capitalize
-	end
-end
+require 'sc2league'
+require 'sc2portrait_style'
+require 'sc2race'
 
 class SC2Stats
-	def scrape
-		html = Nokogiri::HTML(open("#{@url}/ladder/leagues","User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0) Gecko/20100101 Firefox/4.0"))
+  
+  # --------------------------------------------------------------------------
+  #
+  # Accessor
+  #
+  # --------------------------------------------------------------------------
+  
+  attr_reader :name, :race, :points, :league, :portrait_style
+  attr_accessor :url, :options
+  
+  
+  # --------------------------------------------------------------------------
+  #
+  # Constructor method
+  #
+  # --------------------------------------------------------------------------
+  
+  def initialize(profileurl, options=[:name, :race, :points, :league, :portrait_style])
+    @url = profileurl
+    @options = options
+    self.scrape @options
+  end
+  
+  
+  # --------------------------------------------------------------------------
+  #
+  # Public methods
+  #
+  # --------------------------------------------------------------------------
+  
+  def to_s
+    "#{@name} (#{@league}, #{@race}, #{@points} pts)"
+  end
 
-		@league = scrape_league(html)
-		@points = scrape_points(html)
-		@name = scrape_name(html)
+  def ==(o)
+    if o.is_a? SC2Stats
+      @url == o.url
+    else
+      false
+    end
+  end
+  
+  def scrape(options)
+    @url.chop! if @url.last == '/'
+    @url.gsub!(/(.+\/sc2\/)(\w{2})(\/profile\/.+)/, '\1en\3')
+    html = Nokogiri::HTML(open("#{@url}/ladder/leagues"))
 
-		true
-	end
+    options.each do |option|
+      instance_variable_set("@#{option.to_s}".to_sym, self.send("scrape_#{option.to_s}", html))
+    end
+  end
+  
+  
+  # --------------------------------------------------------------------------
+  #
+  # Protected methods
+  #
+  # --------------------------------------------------------------------------
+  
+  protected
+  
+  def scrape_name(html)
+    html.css("#profile-header h2 a").first.text
+  end
+  
+  def scrape_portrait_style(html)
+    raw_portrait_style = html.css("#portrait span").attr('style').to_s
+    return SC2PortraitStyle.new if raw_portrait_style.blank?
+    style_split = raw_portrait_style.split
+    image_index = /.+\/(\d)-90.jpg.+/.match(style_split[1])[1]
+    image_x = style_split[2]
+    image_y = style_split[3]
+    SC2PortraitStyle.new(image_index, image_x.split('px')[0].to_i, image_y.split('px')[0].to_i)
+  end
+  
+  def scrape_race(html)
+    td = html.css("#current-rank td")[current_rank_index_start(html) + 2]
+    return SC2Race.new if td.blank?
+    a = td.css("a")
+    return SC2Race.new if a.blank?
+    class_race = a.attr('class').to_s
+    return SC2Race.new if class_race.blank?
+    race = class_race.split('-')[1]
+    SC2Race.new(race)
+  end
+  
+  def scrape_points(html)
+    td = html.css("#current-rank td")[current_rank_index_start(html) + 3]
+    td.present? ? td.text.to_i : 0
+  end
 
-	def scrape_league(html)
-		league = nil
+  def scrape_league(html, league_type='1v1')
+    league_text = nil
+    
+    html.css("#profile-menu li a").each do |l|
+      if l.text.include? league_type
+        league_text = l.text
+        break
+      end
+    end
 
-		html.css("#profile-menu li a").each do |l|
-			if l.text.include? "1v1"
-				league = l
-				break
-			end
-		end
+    if league_text.present?
+      league_split = league_text.downcase.split
+      league_name = league_split[1]
+      rank = league_split[3].to_i
+    else
+      return SC2League.new
+    end
 
-		leaguename = league.text.strip.gsub(/\dv\d /, "").gsub(/ Rank.+/, "").downcase unless league.nil?
-		leaguename ||= SC2League::LEAGUES.last
-
-		SC2League.new(leaguename)
-	end
-
-	def scrape_points(html)
-		td = html.css("#current-rank td")[3]
-
-		if td.nil?
-			0
-		else
-			Integer(td.text)
-		end
-	end
-
-	def scrape_name(html)
-		html.css("#profile-header h2 a").first.text
-	end
-
-	def initialize(profileurl)
-		@url = profileurl
-
-		self.scrape
-	end
-
-	def url; @url; end
-	def url=(value)
-		@url = value
-	end
-
-	def league; @league; end
-	def points; @points; end
-	def name; @name; end
-
-	def to_s
-		"#{@name} (#{@league}, #{@points} pts)"
-	end
-
-	def ==(o)
-		if o.is_a? SC2Stats
-			@url == o.url
-		else
-			false
-		end
-	end
+    SC2League.new(league_name, league_type, rank)
+  end
+  
+  def current_rank_index_start(html)
+    tds = html.css("#current-rank td")
+    return 0 if tds.blank?
+    tds[0].attr('class').to_s.include?('banner') ? 1 : 0
+  end
 end